744fe22821962345a73480ee9f9ce9392f9a0fc5
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-02-24 11:20:36 +0100

    none

---
+++ src/pages/resume.js
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import Main from "../components/site/layout/main";
+import About from "./../components/site/home/about";
+import TechStack from "./../components/site/home/techstack";
+import Projects from "./../components/site/home/projects";
+import { useSiteMetadata } from "./../components/useSiteMetadata";
+import Divider from "../components/common/divider";
+
+const Resume = () => {
+  const { description } = useSiteMetadata();
+
+  return (
+    <Main>
+      <Divider />
+      <About>{description}</About>
+      <Divider />
+      <TechStack />
+      <Projects />
+    </Main>
+  );
+};
+
+export default Resume;
