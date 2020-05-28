74c686452247f091224cdd9f6383868e1b2b2f7b
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-05-28 15:55:14 +0200

    fix?

--- root-wrapper.js
+++ root-wrapper.js
@@ -40,9 +40,7 @@ const components = {
         </ContentLayout>
       )
     ) : ( (
-          <section >
-            {children}
-          </section>         
+            {children}        
       )
     )
   },
