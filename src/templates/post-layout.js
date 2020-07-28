a2885a3fb38411d7ebe5fac977a0a6f226b51785
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-07-28 21:12:52 +0200

    forgot import

--- src/templates/post-layout.js
+++ src/templates/post-layout.js
@@ -13,7 +13,7 @@ import Twitter from "../components/site/home/twitter";
 import gsap from "gsap";
 import PageTitle from "./../components/common/page-title";
 import Divider from "./../components/common/divider";
-// import getShareImage from "@jlengstorf/get-share-image";
+import getShareImage from "@jlengstorf/get-share-image";
 import { Helmet } from "react-helmet";
 
 const PostLayout = ({ data, pageContext, location }) => {
