90466cc453ebc9abc956816406ad3f1204323bd8
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-07-29 13:54:39 +0200

    offset title

--- src/templates/post-layout.js
+++ src/templates/post-layout.js
@@ -63,7 +63,7 @@ const PostLayout = ({ data, pageContext, location }) => {
     textLeftOffset: 100,
     titleFontSize: 110,
     titleExtraConfig: "_bold",
-    titleBottomOffset: 440,
+    titleBottomOffset: 100,
     titleGravity: "north_west",
     taglineGravity: "north_west",
     titleFont: "Jost.ttf",
